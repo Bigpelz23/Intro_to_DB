d447d08c2d5e1d141e5bde82b635d730925b0693
Bigpelz23 <pehlummie1@gmail.com>
2025-08-14 20:59:02 +0100

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,4 @@
--- Show the full description of the 'Books' table from the 'alx_book_store' database
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store'
-  AND TABLE_NAME = 'Books'
-ORDER BY ORDINAL_POSITION;
+-- Insert a single row into the customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
