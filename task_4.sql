77b2f48b9094003bcb3bc9929b4f4a9e517df664
Bigpelz23 <pehlummie1@gmail.com>
2025-08-14 20:53:48 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,8 @@
 -- Show the full description of the books table
 SHOW CREATE TABLE books;
+
+-- Display full description of the 'Books' table from 'alx_book_store'
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
